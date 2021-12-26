7e3825bbfdc8803c1b4383da1e799f44822d2188
alenamor7 <morozkinalyona@gmail.com>
2021-12-26 22:16:24 +0100

    feat: add startup data initialization

--- pet-clinic-data/src/main/java/com/springframework/springpetclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/com/springframework/springpetclinic/services/map/PetServiceMap.java
@@ -2,10 +2,13 @@ package com.springframework.springpetclinic.services.map;
 
 import com.springframework.springpetclinic.model.Pet;
 import com.springframework.springpetclinic.services.CrudService;
+import com.springframework.springpetclinic.services.PetService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
-public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+@Service
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
 
     @Override
     public Set<Pet> findAll() {
