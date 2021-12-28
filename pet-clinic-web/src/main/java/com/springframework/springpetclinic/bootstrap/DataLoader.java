70589c46afac12db71ff2422f461780e063b69ac
alenamor7 <morozkinalyona@gmail.com>
2021-12-28 13:31:58 +0100

    feat: add visit jpa and map services, visit bootstrap data

--- pet-clinic-web/src/main/java/com/springframework/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/springframework/springpetclinic/bootstrap/DataLoader.java
@@ -1,10 +1,8 @@
 package com.springframework.springpetclinic.bootstrap;
 
 import com.springframework.springpetclinic.model.*;
-import com.springframework.springpetclinic.services.OwnerService;
-import com.springframework.springpetclinic.services.PetTypeService;
-import com.springframework.springpetclinic.services.SpecialtyService;
-import com.springframework.springpetclinic.services.VetService;
+import com.springframework.springpetclinic.repositories.VisitRepository;
+import com.springframework.springpetclinic.services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -17,12 +15,15 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialtyService specialtyService;
+    private final VisitService visitService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService,
+                      SpecialtyService specialtyService, VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialtyService = specialtyService;
+        this.visitService = visitService;
     }
 
     @Override
@@ -76,7 +77,6 @@ public class DataLoader implements CommandLineRunner {
         owner2.setAddress("1 Brickerel");
         owner2.setCity("Miami");
         owner2.setTelephone("456456456");
-        ownerService.save(owner2);
 
         Pet fionasCat = new Pet();
         fionasCat.setPetType(savedCatPetType);
@@ -84,6 +84,13 @@ public class DataLoader implements CommandLineRunner {
         fionasCat.setBirthDate(LocalDate.now());
         fionasCat.setName("Barsik");
         owner2.getPets().add(fionasCat);
+        ownerService.save(owner2);
+
+        Visit catVisit = new Visit();
+        catVisit.setPet(fionasCat);
+        catVisit.setDate(LocalDate.now());
+        catVisit.setDescription("Sneezy kitty");
+        visitService.save(catVisit);
 
         System.out.println("Loaded Owners...");
 
